180c6eb40856cb99493dd6f3cbca93a706a65d2f
Nemcsics Gábor <nemcsics.gabor@gmail.com>
2021-04-30 12:41:25 +0200

    done

--- angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
+++ angular-cinema-project/cinema/src/app/movie-list/movie-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Movie } from '../model/movie';
+import { HttpService } from '../service/http.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,13 +12,20 @@ export class MovieListComponent implements OnInit {
 
   movies: Movie[] = [];
 
-  constructor() { }
+  constructor(
+    private httpService: HttpService,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  getMovies(): void {}
+  getMovies(): void {
+    this.httpService.getMovieList().subscribe(response => this.movies = response);
+  }
 
-  deleteMovie(id: number): any {}
-
+  deleteMovie(id: number): any {
+    return this.httpService.deleteMovie(id).subscribe(
+      response => this.getMovies()
+    )
+  }
 }
